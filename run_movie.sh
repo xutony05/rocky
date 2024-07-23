b13e35fffa8601a4e5481950a15d3cc4edd2ebeb
Tony Xu <xutony05@gmail.com>
2024-07-23 16:23:58 -0400

    graph

--- run_movie.sh
+++ run_movie.sh
@@ -1,6 +1,6 @@
 for recommend_num in 25
 do
-    for sst in neutral age country gender continent occupation race religion physics
+    for sst in typo
     do
         echo $sst
         python3 -u ./movie/run.py \
